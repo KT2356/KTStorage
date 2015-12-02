ddce03b032486e8a7a7770c8ab04f9e7284bc232
KT2356 <15152112356@163.com>
2015-12-02 09:55:47 +0800

    Foundation of Project

---
+++ KTStorageDemo/KTStorageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KTStorageDemo
+//
+//  Created by KT on 15/12/2.
+//  Copyright © 2015年 KT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
